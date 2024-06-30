918069ca010ba03076c362518b015b6bf2115a85
Lucas Gonçalves <lucasgnclvs1@gmail.com>
2024-06-30 10:48:31 -0300

    Adding middleware json control and possibility for push new data in livros route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
  import express from "express";
 
  const app = express();
+ app.use(express.json());
 
  const livros = [
     {
-    id: 1,
-    titulo: "O Senhor dos Anéis"
+        id: 1,
+        titulo: "O Senhor dos Anéis"
     },
     {
         id: 2,
@@ -20,5 +21,11 @@
  app.get("/livros", (req, res) => {
     res.status(200).json(livros);
  });
+
+ app.post("/livros", (req, res) => {
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso");
+ });
+
 
  export default app;
